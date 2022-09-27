3b0e587782f0e37aae1d1bdac261224e56364e8a
RaulAgreda <100451269@alumnos.uc3m.es>
2022-09-27 17:33:05 +0200

    ex018 and ex067 :D

---
+++ ex018.py
@@ -0,0 +1,30 @@
+class MaxTrianglePath:
+	def __init__(self, file):
+		self.cache = {}
+		self.M = self._extract_matrix(file)
+
+	def _extract_matrix(self, file):
+		matrix = []
+		fd = open(file, 'r')
+		lines = fd.read().split('\n')
+		fd.close()
+		for i in range(len(lines)):
+			nums = lines[i].split(' ')
+			matrix.append([])
+			for n in nums:
+				matrix[i].append(int(n))
+		return (matrix)
+
+	def get_path(self, level, position):
+		if ((level, position) in self.cache):
+			return self.cache[(level, position)]
+		if (level == len(self.M) - 1):
+			return self.M[level][position]
+		total = self.M[level][position]
+		total1 = total + self.get_path(level + 1, position)
+		total2 = total + self.get_path(level + 1, position + 1)
+		self.cache[(level, position)] = total1 if total1 > total2 else total2
+		return self.cache[((level, position))]
+
+if (__name__ == "__main__"):
+	print(MaxTrianglePath("ex018.txt").get_path(0, 0))
---
+++ ex067.py
@@ -0,0 +1,4 @@
+from ex018 import MaxTrianglePath
+
+if (__name__ == "__main__"):
+	print(MaxTrianglePath("ex067.txt").get_path(0, 0))
